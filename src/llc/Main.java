088c2049fe2d4a970d87bfa80113efacf9e432ae
websitenmaker12 <eidechse12@hotmail.com>
2014-08-11 13:58:02 +0200

    Working on Main-Class

--- src/llc/Main.java
+++ src/llc/Main.java
@@ -1,8 +1,31 @@
 package llc;
 
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.ContextCapabilities;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GLContext;
+import org.lwjgl.opengl.PixelFormat;
+
 public class Main {
 
 	public static void main(String[] args) {
+		try {
+			//Display.setDisplayMode(new DisplayMode(640, 480));
+			//Display.setResizable(false);
+			ContextCapabilities c = GLContext.getCapabilities();
+			
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+		
+		while(!Display.isCloseRequested()) {
+		}
+		
+		if(Display.isCreated()) Display.destroy();
+		System.exit(0);
 	}
 
 }
