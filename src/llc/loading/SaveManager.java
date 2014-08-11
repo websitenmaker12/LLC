9bbcea0564517a141f75e25fe50796084e8f4e77
simolus3 <Simon@Binder-online.biz>
2014-08-11 14:05:26 +0200

    Default path for saving files added

--- src/llc/loading/SaveManager.java
+++ src/llc/loading/SaveManager.java
@@ -1,6 +1,24 @@
 package llc.loading;
 
+import java.io.File;
+
 public class SaveManager {
 	
+	private static File safePath;
 	
+	static {
+		String homeDir = System.getProperty("user.home"); //Home of the user data on the hard drive
+		
+		homeDir += "/LLC/saves";
+		
+		safePath = new File(homeDir);
+	}
+	
+	public static void saveToFile(SaveGame f) {
+		
+	}
+	
+	public static SaveGame loadFromFile() {
+		return null;
+	}
 }
