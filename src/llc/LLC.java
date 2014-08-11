4d1912d69ee9a6eefc6fa4f292ff1b3b87dcf0a5
websitenmaker12 <eidechse12@hotmail.com>
2014-08-11 14:27:48 +0200

    Added LLC.class

---
+++ src/llc/LLC.java
@@ -0,0 +1,23 @@
+package llc;
+
+public class LLC {
+
+	public LLC() {
+	}
+	
+	public void startGame() {
+		this.initDisplay();
+		this.initOpenGL();
+		this.beginLoop();
+	}
+	
+	public void initDisplay() {
+	}
+	
+	public void initOpenGL() {
+	}
+	
+	public void beginLoop() {
+	}
+	
+}
