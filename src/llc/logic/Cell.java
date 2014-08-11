d15c458e81aae6f1246b5cc98ae8167a967a53b9
wara286LP <wara286LP@gmail.com>
2014-08-11 19:20:25 +0200

    Working on cell.java

--- src/llc/logic/Cell.java
+++ src/llc/logic/Cell.java
@@ -1,5 +1,41 @@
 package llc.logic;
 
-public class Cell {
+import llc.entity.Entity;
 
+public class Cell {
+	
+	private Entity entity;
+	private CellType type;
+	
+	/***
+	 * Gets the {@link Entity} that's on the cell.
+	 * @return	the cell's entity
+	 */
+	public Entity getEntity() {
+		return entity;
+	}
+	
+	/***
+	 * Sets the {@link Entity} on the cell.
+	 * @param {@link Entity} to set
+	 */
+	public void setEntity(Entity entity) {
+		this.entity = entity;
+	}
+	
+	/***
+	 * Gets the {@link CellType}}
+	 * @return The entity
+	 */
+	public CellType getType() {
+		return type;
+	}
+	
+	/***
+	 * Sets the {@link CellType}
+	 * @param type
+	 */
+	public void setType(CellType type) {
+		this.type = type;
+	}
 }
