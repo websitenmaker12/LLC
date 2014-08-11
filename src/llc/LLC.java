8182e0901825f066694d344fe86158fcd8d5363d
websitenmaker12 <eidechse12@hotmail.com>
2014-08-11 17:00:30 +0200

    Added Profiler

--- src/llc/LLC.java
+++ src/llc/LLC.java
@@ -1,23 +1,53 @@
 package llc;
 
+import llc.engine.Profiler;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+
 public class LLC {
 
+	public static final String VERSION = "0.1 INDEV";
+	private boolean isRunning = false;
+	
+	private Profiler profiler = new Profiler();
+	
 	public LLC() {
 	}
 	
-	public void startGame() {
+	public void startGame() throws LWJGLException {
+		this.profiler.start("Setup Display");
 		this.initDisplay();
+		this.profiler.endStart("Setup OpenGL");
 		this.initOpenGL();
+		this.profiler.end();
 		this.beginLoop();
 	}
 	
-	public void initDisplay() {
+	public void initDisplay() throws LWJGLException {
+		Display.setDisplayMode(new DisplayMode(640, 480));
+		Display.setResizable(false);
+		Display.setTitle("LLC - " + VERSION);
+		Display.create();
 	}
 	
 	public void initOpenGL() {
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, Display.getWidth(), Display.getHeight(), 0, -1, 1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		GL11.glLoadIdentity();
+		
+		GL11.glClearColor(0F, 0F, 0F, 1F);
 	}
 	
 	public void beginLoop() {
+		this.isRunning = true;
+		while(this.isRunning) {
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+		}
 	}
 	
 }
