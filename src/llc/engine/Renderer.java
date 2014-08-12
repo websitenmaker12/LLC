459b161c02e4000881b0762deeea1ac0c414fa56
taker314 <madeheim.t@lmgbb.bw.lo-net2.de>
2014-08-12 17:29:29 +0200

    Started implementing grid rendering

--- src/llc/engine/Renderer.java
+++ src/llc/engine/Renderer.java
@@ -1,5 +1,7 @@
 package llc.engine;
 
+import llc.logic.Cell;
+import llc.logic.CellType;
 import llc.logic.GameState;
 
 import org.lwjgl.opengl.GL11;
@@ -21,11 +23,33 @@ public class Renderer {
 		GL11.glLoadIdentity();
 	}
 	
-	public void render(Camera camera, GameState gameState) {
+	public void render(Camera camera, GameState gameState) 
+	{
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-		GL11.glLoadIdentity();
-		
+		GL11.glLoadIdentity();		
 		GL11.glTranslatef(camera.pos.x, camera.pos.y, camera.pos.z);
+		
+		Cell[][] cells = gameState.getGrid().getCells();
+		GL11.glBegin(GL11.GL_TRIANGLES);
+		GL11.glNormal3f(0, 0, 1);
+		for (int y = 0; y > cells.length; y++)
+		{
+			for (int x = 0; x > cells[0].length; x++)
+			{
+				if (cells[y][x].getType() == CellType.SOLID)
+				{
+					
+				}
+				GL11.glTexCoord2d(0, 0); GL11.glVertex2d(x, y);
+				GL11.glTexCoord2d(1, 0); GL11.glVertex2d(x + 1, y);
+				GL11.glTexCoord2d(0, 1); GL11.glVertex2d(x, y + 1);
+				
+				GL11.glTexCoord2d(1, 0); GL11.glVertex2d(x + 1, y);
+				GL11.glTexCoord2d(1, 1); GL11.glVertex2d(x + 1, y + 1);
+				GL11.glTexCoord2d(0, 1); GL11.glVertex2d(x, y + 1);
+
+			}
+		}
+		GL11.glEnd();
 	}
-	
 }
