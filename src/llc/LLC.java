6a68a23c9af4cc1937e2616be609edff7e4e9d96
websitenmaker12 <eidechse12@hotmail.com>
2014-08-12 11:38:04 +0200

    Fixed failed sync

--- src/llc/LLC.java
+++ src/llc/LLC.java
@@ -1,6 +1,8 @@
 package llc;
 
+import llc.engine.Camera;
 import llc.engine.Profiler;
+import llc.util.RenderUtil;
 
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
@@ -9,12 +11,20 @@ import org.lwjgl.opengl.GL11;
 
 public class LLC {
 
+	public static LLC instance;
+	
 	public static final String VERSION = "0.1 INDEV";
 	private boolean isRunning = false;
 	
 	private Profiler profiler = new Profiler();
+	private Camera camera;
+	
+	public int width = 0;
+	public int height = 0;
 	
 	public LLC() {
+		instance = this;
+		this.camera = new Camera(0, 0);
 	}
 	
 	public void startGame() throws LWJGLException {
@@ -46,8 +56,25 @@ public class LLC {
 	public void beginLoop() {
 		this.isRunning = true;
 		while(this.isRunning) {
+			if(Display.isCloseRequested()) this.isRunning = false;
 			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+			GL11.glLoadIdentity();
+			
+			this.profiler.start("Input updates");
+			// TODO implement
+			this.profiler.endStart("Render updates");
+			GL11.glLoadIdentity();
+			this.camera.transformMatrix();
+			RenderUtil.drawQuad(0, 0, 100, 100);
+			this.profiler.end();
+			
+			Display.update();
+			Display.sync(60);
 		}
+		
+		if(Display.isCreated()) Display.destroy();
+		System.out.println(0);
 	}
 	
 }
+
--- src/llc/Main.java
+++ src/llc/Main.java
@@ -1,25 +1,17 @@
 package llc;
 
 import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
 
 public class Main {
 
 	public static void main(String[] args) {
+		LLC game = new LLC();
 		try {
-			Display.setDisplayMode(new DisplayMode(640, 480));
-			Display.setResizable(false);
-			Display.create();
+			game.startGame();
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 		}
-		
-		while(!Display.isCloseRequested()) {
-		}
-		
-		if(Display.isCreated()) Display.destroy();
-		System.exit(0);
 	}
 
 }
+
