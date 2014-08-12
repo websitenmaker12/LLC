28e78927762758b70157419b15d05f61235887e9
websitenmaker12 <eidechse12@hotmail.com>
2014-08-12 12:17:29 +0200

    Code organization improvement

---
+++ src/llc/engine/Renderer.java
@@ -0,0 +1,34 @@
+package llc.engine;
+
+import llc.loading.GameState;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.GL11;
+
+public class Renderer {
+
+	public Renderer() {
+		this.handleDisplayResize();
+		
+		GL11.glClearColor(0F, 0F, 0F, 1F);
+		
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+	}
+	
+	public void handleDisplayResize() {
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, Display.getWidth(), Display.getHeight(), 0, -1, 1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		GL11.glLoadIdentity();
+	}
+	
+	public void render(Camera camera, GameState gameState) {
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+		GL11.glLoadIdentity();
+		
+		GL11.glTranslatef(camera.pos.x, camera.pos.y, camera.pos.z);
+	}
+	
+}
