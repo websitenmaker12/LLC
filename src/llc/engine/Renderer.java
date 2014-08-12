b1c4edb92a76f1e01a49fe0cb04b2494c6a482ed
taker314 <madeheim.t@lmgbb.bw.lo-net2.de>
2014-08-12 17:52:54 +0200

    Added difference between Terrain and Entity

--- src/llc/engine/Renderer.java
+++ src/llc/engine/Renderer.java
@@ -10,6 +10,7 @@ import org.lwjgl.util.glu.GLU;
 
 public class Renderer {
 
+	private TextureLoader loader;
 	public Renderer(TextureLoader textureLoader) {
 		GL11.glClearColor(0F, 0F, 0F, 1F);
 		
@@ -18,6 +19,8 @@ public class Renderer {
 	
 		GL11.glEnable(GL11.GL_DEPTH_TEST);
 		GL11.glDepthFunc(GL11.GL_LEQUAL);
+		
+		loader = textureLoader;
 	}
 	
 	/**
@@ -38,18 +41,40 @@ public class Renderer {
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 		GL11.glLoadIdentity();
 		GL11.glTranslatef(camera.pos.x, camera.pos.y, camera.pos.z);
-		
+		// Render grid
 		Cell[][] cells = gameState.getGrid().getCells();
-		GL11.glBegin(GL11.GL_TRIANGLES);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
 		GL11.glNormal3f(0, 0, 1);
 		for (int y = 0; y > cells.length; y++)
 		{
 			for (int x = 0; x > cells[0].length; x++)
 			{
-				if (cells[y][x].getType() == CellType.SOLID)
-				{
+				if (cells[y][x].getEntity() == null){
+					//Render terrain texture
+					if (cells[y][x].getType() == CellType.SOLID)
+					{
+						loader.getTexture("SOLID").bind();
+					}
+					if (cells[y][x].getType() == CellType.WALKABLE)
+					{
+						loader.getTexture("WALKABLE").bind();
+					}
 					
 				}
+				else
+				{
+					//Render entity texture
+					//No Entity texture loaded at the moment
+//					if (cells[y][x].getEntity(). == CellType.SOLID)
+//					{
+//						loader.getTexture("SOLID").bind();
+//					}
+//					if (cells[y][x].getEntity() == CellType.WALKABLE)
+//					{
+//						loader.getTexture("WALKABLE").bind();
+//					}
+				}
+				GL11.glBegin(GL11.GL_TRIANGLES);
 				GL11.glTexCoord2d(0, 0); GL11.glVertex2d(x, y);
 				GL11.glTexCoord2d(1, 0); GL11.glVertex2d(x + 1, y);
 				GL11.glTexCoord2d(0, 1); GL11.glVertex2d(x, y + 1);
@@ -58,8 +83,10 @@ public class Renderer {
 				GL11.glTexCoord2d(1, 1); GL11.glVertex2d(x + 1, y + 1);
 				GL11.glTexCoord2d(0, 1); GL11.glVertex2d(x, y + 1);
 
+				GL11.glEnd();
+				
 			}
 		}
-		GL11.glEnd();
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
 	}
 }
