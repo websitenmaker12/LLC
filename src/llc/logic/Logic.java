ffa7daa437aed8f6974bcfaf5d79efd8ec85833a
PetaByteBoy <petabyteboy@gmail.com>
2014-08-12 12:17:24 +0200

    Added stuff to logic.class

--- src/llc/logic/Logic.java
+++ src/llc/logic/Logic.java
@@ -1,8 +1,21 @@
 package llc.logic;
 
+import llc.loading.GameState;
+import llc.loading.Grid;
+
 public class Logic {
-	
-	
-	
-	
+
+	private GameState gameState;
+
+	public Logic(Grid grid) {
+		this.setGameState(new GameState(grid));
+	}
+
+	public GameState getGameState() {
+		return gameState;
+	}
+
+	public void setGameState(GameState gameState) {
+		this.gameState = gameState;
+	}
 }
