ff5dd8e61c794a30c23f4210ee4a91fc56a6f9a6
simolus3 <Simon@Binder-online.biz>
2014-08-13 14:04:23 +0200

    GameState edited

--- src/llc/logic/GameState.java
+++ src/llc/logic/GameState.java
@@ -5,8 +5,9 @@ public class GameState {
 	private Grid grid;
 	
 	private Player player1, player2;
-
-	private Player activePlayer;
+	private Cell townHall1, townHall2;
+	
+	private int activePlayer;
 	public int moveCount = 0;
 	
 	public boolean isGameOver = false;
@@ -24,12 +25,14 @@ public class GameState {
 		return grid;
 	}
 
-	public Player getActivePlayer() {
-		return activePlayer;
-	}
-
-	public void setActivePlayer(Player activePlayer) {
-		this.activePlayer = activePlayer;
+	public void setActivePlayer(Player active) {
+		if (player1.equals(activePlayer)) {
+			activePlayer = 1;
+		} else if (player2.equals(activePlayer)) {
+			activePlayer = 2;
+		} else {
+			throw new IllegalArgumentException("Given Player argument does not exist");
+		}
 	}
 
 	public Player getPlayer1() {
@@ -42,7 +45,31 @@ public class GameState {
 	public Player getActivePlayer() {
 		if (activePlayer == 1) {
 			return player1;
+		} else {
+			return player2;
 		}
-		return player2;
+	}
+	public Player getInActivePlayer() {
+		if (activePlayer == 1) {
+			return player2;
+		} else {
+			return player1;
+		}
+	}
+
+	public Cell getTownHall1Cell() {
+		return townHall1;
+	}
+
+	public void setTownHall1Cell(Cell townHall1) {
+		this.townHall1 = townHall1;
+	}
+
+	public Cell getTownHall2Cell() {
+		return townHall2;
+	}
+
+	public void setTownHall2Cell(Cell townHall2) {
+		this.townHall2 = townHall2;
 	}
 }
