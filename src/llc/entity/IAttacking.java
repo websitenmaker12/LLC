9b7c0d197fd19ec10887aeef9186264499b48c2c
wara286LP <wara286LP@gmail.com>
2014-08-13 11:39:23 +0200

    Added attack damage and working on buildings

--- src/llc/entity/IAttacking.java
+++ src/llc/entity/IAttacking.java
@@ -1,5 +1,13 @@
 package llc.entity;
 
+/**
+ * @author MaxiHoeve14
+ */
 public interface IAttacking {
 	
+	/**
+	 * Gets the amount of damage that the {@link Entity} deals to an enemy {@link Entity}.
+	 * @return Damage
+	 */
+	int getAttackDamage();
 }
