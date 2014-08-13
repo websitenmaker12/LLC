45002ada45c00a4267f44b1ad10134b3eb943000
websitenmaker12 <eidechse12@hotmail.com>
2014-08-13 12:46:52 +0200

    Continue on buttons

--- src/llc/engine/gui/GUIButton.java
+++ src/llc/engine/gui/GUIButton.java
@@ -1,10 +1,12 @@
 package llc.engine.gui;
 
 import llc.engine.GUIRenderer;
+import llc.engine.res.Texture;
 import llc.util.RenderUtil;
 
 import org.lwjgl.input.Mouse;
 import org.newdawn.slick.Color;
+import org.newdawn.slick.opengl.TextureImpl;
 
 public class GUIButton extends GUIElement {
 
@@ -14,6 +16,16 @@ public class GUIButton extends GUIElement {
 	
 	private boolean isHover = false;
 	private boolean isClicked = false;
+	
+	private static final Texture button;
+	private static final Texture buttonHover;
+	private static final Texture buttonDown;
+	
+	static {
+		button = new Texture("res/gui/button_normal.png");
+		buttonHover = new Texture("res/gui/button_hover.png");
+		buttonDown = new Texture("res/gui/button_down.png");
+	}
 	
 	public GUIButton(float posX, float posY, float width, float height, String text) {
 		super(posX, posY);
@@ -31,6 +43,7 @@ public class GUIButton extends GUIElement {
 	@Override
 	public void render(GUIRenderer renderer, int x, int y) {
 		RenderUtil.drawQuad(this.posX, this.posY, this.width, this.height);
+		TextureImpl.bindNone();
 		renderer.arial.drawString(this.posX + this.width / 2 - renderer.arial.getWidth(this.text) / 2,
 				this.posY + this.height / 2 - renderer.arial.getHeight() / 2, this.text, Color.black);
 	}
--- src/llc/util/RenderUtil.java
+++ src/llc/util/RenderUtil.java
@@ -13,5 +13,17 @@ public class RenderUtil {
 		GL11.glEnd();
 	}
 	
+	public static void drawTexturedQuad(double x, double y, double width, double height) {
+		GL11.glBegin(GL11.GL_TRIANGLES);
+			GL11.glTexCoord2f(0, 0); GL11.glVertex2d(x		  , y);
+			GL11.glTexCoord2f(1, 0); GL11.glVertex2d(x + width, y);
+			GL11.glTexCoord2f(0, 1); GL11.glVertex2d(x		  , y + height);
+			
+			GL11.glTexCoord2f(1, 0); GL11.glVertex2d(x + width, y);
+			GL11.glTexCoord2f(1, 1); GL11.glVertex2d(x + width, y + height);
+			GL11.glTexCoord2f(0, 1); GL11.glVertex2d(x		  , y + height);
+		GL11.glEnd();
+	}
+	
 }
 
