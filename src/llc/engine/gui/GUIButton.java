274cdd9de3e79bf3a984d1ac67b93e5e68404aba
websitenmaker12 <eidechse12@hotmail.com>
2014-08-13 14:13:37 +0200

    Finished GUIButton

--- src/llc/engine/gui/GUIButton.java
+++ src/llc/engine/gui/GUIButton.java
@@ -5,10 +5,11 @@ import llc.engine.res.Texture;
 import llc.util.RenderUtil;
 
 import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.GL11;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.opengl.TextureImpl;
 
-public class GUIButton extends GUIElement {
+public abstract class GUIButton extends GUIElement {
 
 	private float width;
 	private float height;
@@ -36,21 +37,29 @@ public class GUIButton extends GUIElement {
 
 	@Override
 	public void update(int x, int y) {
+		if(this.isClicked) this.onClick(x, y);
+		
 		this.isHover = x >= this.posX && x <= this.posX + this.width && y >= this.posY && y <= this.posY + this.height;
 		this.isClicked = this.isHover && Mouse.isButtonDown(0);
 	}
 
 	@Override
 	public void render(GUIRenderer renderer, int x, int y) {
-		RenderUtil.drawTexturedQuad(this.posX, this.posY, this.width, this.height);
-		
-		TextureImpl.bindNone();
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
 		if(this.isClicked) buttonDown.bind();
 		else if(this.isHover) buttonHover.bind();
 		else button.bind();
+		RenderUtil.drawTexturedQuad(this.posX, this.posY, this.width, this.height);
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
 		
+		TextureImpl.bindNone();
 		renderer.arial.drawString(this.posX + this.width / 2 - renderer.arial.getWidth(this.text) / 2,
 				this.posY + this.height / 2 - renderer.arial.getHeight() / 2, this.text, Color.black);
 	}
 
+	/**
+	 * Gets triggered when the button gets clicked
+	 */
+	public abstract void onClick(int x, int y);
+	
 }
