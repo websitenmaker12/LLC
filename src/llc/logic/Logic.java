1ddfe01bdda54e6621a3c90307bc5a4112a7cd35
PetaByteBoy <petabyteboy@gmail.com>
2014-08-15 16:50:47 +0200

    derp

--- src/llc/logic/Logic.java
+++ src/llc/logic/Logic.java
@@ -165,6 +165,7 @@ public class Logic {
 			gameState.setActivePlayer(gameState.getInActivePlayer());
 			gameState.moveCount = 0;
 			gameState.selectedCell = null;
+			selectedEntity = null;
 		}
 	}
 
