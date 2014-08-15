b8a839bbd7465edc5cf1403aa02c881dcaffed86
wara286LP <wara286LP@gmail.com>
2014-08-15 11:41:45 +0200

    GUI work

--- src/llc/engine/gui/GUIIngameMenu.java
+++ src/llc/engine/gui/GUIIngameMenu.java
@@ -19,6 +19,11 @@ public class GUIIngameMenu extends GUI{
 	public void onOpen() {
 		super.onOpen();
 		
+		this.elements.add(new GUIButton(this, Display.getWidth() / 2 - 100, Display.getHeight() / 2 - 165, 200, 35, "Save") {
+			public void onClick(int x, int y) {
+			}
+		});
+		
 		this.elements.add(new GUIButton(this, Display.getWidth() / 2 - 100, Display.getHeight() / 2 - 110, 200, 35, "Save") {
 			public void onClick(int x, int y) {
 				gameLoader.saveToFile(logic.getGameState(), "save.llcsav");
