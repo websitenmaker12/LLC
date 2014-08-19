9d9be0269d6220d93f244d472445822e30e03720
websitenmaker12 <eidechse12@hotmail.com>
2014-08-19 11:58:36 +0200

    New GUI

--- src/llc/engine/gui/GUIIngame.java
+++ src/llc/engine/gui/GUIIngame.java
@@ -1,6 +1,5 @@
 package llc.engine.gui;
 
-import llc.LLC;
 import llc.engine.GUIRenderer;
 import llc.engine.res.Texture;
 import llc.entity.EntityWarrior;
@@ -17,75 +16,64 @@ public class GUIIngame extends GUI {
 
 	protected Logic logic;
 	
-	private Texture left;
-	private Texture middle;
-	private Texture right;
+	private Texture ui;
 	
 	public GUIIngame(Logic logic, GameLoader gameLoader) {
 		this.logic = logic;
 		
-		left = new Texture("res/gui/ingame_left.png");
-		middle = new Texture("res/gui/ingame_middle.png");
-		right = new Texture("res/gui/ingame_right.png");
+		this.ui = new Texture("res/gui/ui.png");
 	}
 	
 	@Override
 	public void onOpen() {
 		super.onOpen();
 		
-		this.elements.add(new GUIButton(this, Display.getWidth() - 212, Display.getHeight() - 55, 200, 35, "Buy Warrior") {
-			public void onClick(int x, int y) {
-				logic.buyEntity(new EntityWarrior());
-			}
-		});
-		
-		this.elements.add(new GUIButton(this, Display.getWidth() - 212, Display.getHeight() - 110, 200, 35, "Buy Worker") {
-			public void onClick(int x, int y) {
-				logic.buyEntity(new EntityWorker());
-			}
-		});
-		
-		this.elements.add(new GUIText(this, 50, 20, 0, 0, "Gold: ", Color.orange) {
-			@Override
-			public void update(int x, int y) {
-				setText("Gold:" + logic.getGameState().getActivePlayer().getMinerals());
-				if (logic.markMinerals) {
-					this.mark(Color.red, 1500);
-					logic.markMinerals = false;
-				}
-			}
-		});
-		
-		this.elements.add(new GUIText(this, Display.getWidth() - 60, 20, 0, 0, "Player: ", Color.orange) {
-			@Override
-			public void update(int x, int y) {
-				setText("Player " + logic.getGameState().getActivePlayer().playerID);
-			}
-		});
-		
-		this.elements.add(new GUIText(this, Display.getWidth() / 2 , 20, 0, 0, "Turns left: ", Color.orange) {
-			@Override
-			public void update(int x, int y) {
-				setText("Turns left: " + (logic.subTurns - logic.getGameState().moveCount) + "/" + logic.subTurns);
-			}
-		});
+//		this.elements.add(new GUIButton(this, Display.getWidth() - 212, Display.getHeight() - 55, 200, 35, "Buy Warrior") {
+//			public void onClick(int x, int y) {
+//				logic.buyEntity(new EntityWarrior());
+//			}
+//		});
+//		
+//		this.elements.add(new GUIButton(this, Display.getWidth() - 212, Display.getHeight() - 110, 200, 35, "Buy Worker") {
+//			public void onClick(int x, int y) {
+//				logic.buyEntity(new EntityWorker());
+//			}
+//		});
+//		
+//		this.elements.add(new GUIText(this, 50, 20, 0, 0, "Gold: ", Color.orange) {
+//			@Override
+//			public void update(int x, int y) {
+//				setText("Gold:" + logic.getGameState().getActivePlayer().getMinerals());
+//				if (logic.markMinerals) {
+//					this.mark(Color.red, 1500);
+//					logic.markMinerals = false;
+//				}
+//			}
+//		});
+//		
+//		this.elements.add(new GUIText(this, Display.getWidth() - 60, 20, 0, 0, "Player: ", Color.orange) {
+//			@Override
+//			public void update(int x, int y) {
+//				setText("Player " + logic.getGameState().getActivePlayer().playerID);
+//			}
+//		});
+//		
+//		this.elements.add(new GUIText(this, Display.getWidth() / 2 , 20, 0, 0, "Turns left: ", Color.orange) {
+//			@Override
+//			public void update(int x, int y) {
+//				setText("Turns left: " + (logic.subTurns - logic.getGameState().moveCount) + "/" + logic.subTurns);
+//			}
+//		});
 	}
 	
 	@Override
 	public void render(GUIRenderer renderer, int x, int y) {
-		float scale = (float)Display.getWidth() / (float)LLC.standartDisplayMode.getWidth();
-		
 		GL11.glPushMatrix();
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
 		
-		left.bind();
-		RenderUtil.drawTexturedQuad(0, Display.getHeight() - 130F * scale, 130F * scale, 130F * scale);
-		
-		right.bind();
-		RenderUtil.drawTexturedQuad(Display.getWidth() - 200F * scale, Display.getHeight() - 130F * scale, 200F * scale, 130F * scale);
-		
-		middle.bind();
-		RenderUtil.drawTexturedQuad(130F * scale, Display.getHeight() - 94F * scale, 310F * scale, 94F * scale);
+		// UI Texture
+		this.ui.bind();
+		RenderUtil.drawTexturedQuad(0, 0, Display.getWidth(), Display.getHeight());
 		
 		GL11.glDisable(GL11.GL_TEXTURE_2D);
 		GL11.glPopMatrix();
